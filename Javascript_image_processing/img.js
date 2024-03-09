8435c14d0cef194220cb49b48a667fd47f5d5354
Gabriel_Ghita <128585958+gabriel16231@users.noreply.github.com>
2024-03-09 10:42:47 +0200

    img processing added

--- Javascript_image_processing/img.js
+++ Javascript_image_processing/img.js
@@ -1,41 +1,35 @@
+let btn = document.getElementById("btnClick");
+      const img = new Image();
+      img.crossOrigin = "anonymous";
 
-const img = new Image();
-img.crossOrigin = "Anonymous";
-const apiKey = 'live_D0aAVyHStAR6lUwaNwJc543aOQmICaVOSALFwgkvgaOVytidugJXgkOVUcsoCe8O'; // Replace with your CatAPI key
-const catApiEndpoint = 'https://api.thecatapi.com/v1/images/search';
-const catCanvas = document.getElementById('canvas');
-const ctx = catCanvas.getContext('2d');
+      btn.addEventListener("click", async function () {
+        try {
+          const response = await fetch(
+            "https://dog.ceo/api/breeds/image/random"
+          );
+          const result = await response.json();
 
-// Function to fetch and draw a random cat image
-function fetchAndDrawCat() {
-    fetch(catApiEndpoint, { headers: { 'x-api-key': apiKey } })
-        .then(response => response.json())
-        .then(data => {
-            const imageUrl = data[0].url;
+          const canvas = document.getElementById("canvas");
+          const ctx = canvas.getContext("2d");
+          console.log(result);
 
-           
-            img.src = imageUrl;
-            catCanvas.height=img.naturalHeight;
-            catCanvas.width=img.naturalWidth;
-            img.onload = function () {
-                ctx.clearRect(0, 0, catCanvas.width, catCanvas.height); // Clear canvas
-                ctx.drawImage(img, 0, 0, catCanvas.width, catCanvas.height);
-            };
-        })
-        .catch(error => console.error('Error fetching cat image:', error));
-}
+          img.onload = () => {
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+          };
 
-// Add click event listener to the "Fetch Random Cat" button
-document.getElementById('btnClick').addEventListener('click', fetchAndDrawCat);
-
-
-
+          img.src = result.message;
+        } catch (err) {
+          console.log(err);
+        }
+      });
 
       let btn_flip = document.getElementById("btnmirror");
 
       btn_flip.addEventListener("click", function () {//va oglindi imaginea salvata cu img 
         const start = performance.now();
-        mirror_with_px("canvas");
+        mirror_with_px("canvas_mirror");
         const end = performance.now();
         console.log(`Timp de executie: mirroring -> ${end - start} ms.`);
       });
@@ -44,55 +38,98 @@ document.getElementById('btnClick').addEventListener('click', fetchAndDrawCat);
 
       btn_g_s.addEventListener("click", function () {//convert to gray scale
         const start = performance.now();
-        gray_scale_img("canvas");
+        gray_scale_img("canvas_mirror");
         const end = performance.now();
         console.log(`Timp de executie: gray scale -> ${end - start} ms.`);
       });
 
       let btn_b = document.getElementById("btnb");
 
-/*
+      btn_b.addEventListener("click", function () {//convert to binary image
+        const start = performance.now();
+        binary_img("canvas_mirror");
+        const end = performance.now();
+        console.log(
+          `Timp de executie: gray scale to binary -> ${end - start} ms.`
+        );
+      });
+
+      function mirror_img(canvasID) {
+        const canvas = document.getElementById(canvasID);
+        const ctx = canvas.getContext("2d");
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+        ctx.save();     //salvam canvas-ul inainte de ii da restore dupa modificari
+        ctx.scale(-1, 1);  //oglindeste continutul canvasului
+        ctx.drawImage(img, 0, 0, -img.width, img.height);
+        ctx.restore();
+      }
+
       function gray_scale_img(canvasID) {
+        const canvas = document.getElementById(canvasID);
+        const ctx = canvas.getContext("2d");
 
         const img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
         const scanned_data = img_data.data;
         console.log(img_data);
-        for(let i=0;i<canvas.height;i+=1)
-        {
-          let index=i*canvas.width*4;
-        for(let j=0;j<canvas.width/2;j+=1) {
-          let poz=index+4*j;
+        for (let i = 0; i < scanned_data.length; i += 4) {
             //pt a face conversie la gray scale trb sa fasim media valorilor rgb si sa dam tuturor aceasta valore medie
             //pt ca un pixel sa fie gri trb ca val R G B sa fie egale 
           const avgc =
-            (scanned_data[poz] + scanned_data[poz + 1] + scanned_data[poz + 2]) / 3;
+            (scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]) / 3;
 
-          scanned_data[poz] = avgc;
-          scanned_data[poz + 1] = avgc;
-          scanned_data[poz + 2] = avgc;
+          scanned_data[i] = avgc;
+          scanned_data[i + 1] = avgc;
+          scanned_data[i + 2] = avgc;
         }
-      }
 
         img_data.data = scanned_data;
         ctx.putImageData(img_data, 0, 0);
       }
-      
 
+      function binary_img(canvasID) {
+        const canvas = document.getElementById(canvasID);
+        const ctx = canvas.getContext("2d");
+
+        const img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const scanned_data = img_data.data;
+        console.log(img_data);
+        for (let i = 0; i < scanned_data.length; i += 4) {
+          let avgc =
+            (scanned_data[i] + scanned_data[i + 1] + scanned_data[i + 2]) / 3;
+          if (avgc > 105) {//am ales un treshold static a i daca valoarea medie este mai mare ca treshold ul pixelul devine alb[255,255,255] altfel devine negru [0,0,0]
+            avgc = 255;
+          } else {
+            avgc = 0;
+          }
+          scanned_data[i] = avgc;
+          scanned_data[i + 1] = avgc;
+          scanned_data[i + 2] = avgc;
+        }
+        //obs: aceasta functie transforma orice img (indiferent daca este gray scale sau nu ) intr una binara
+        img_data.data = scanned_data;
+        ctx.putImageData(img_data, 0, 0);
+      }
 
 
       function mirror_with_px(canvasID)
       {
+        const canvas = document.getElementById(canvasID);
+        const ctx = canvas.getContext("2d");
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
 
+        ctx.drawImage(img,0,0)
         const img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
         const scanned_data = img_data.data;
 
         for(let i=0;i<canvas.height;i+=1)
         {
           let index=i*canvas.width*4;
-        for(let j=canvas.width/2;j<(3*canvas.width)/4;j+=1)
+        for(let j=0;j<canvas.width/2;j+=1)
         {
           let poz_c=index+4*j;
-          let poz_m=index+(canvas.width/2-j)*4;
+          let poz_m=index+(canvas.width-j)*4;
           
           
           let c=scanned_data[poz_c];
@@ -112,7 +149,7 @@ document.getElementById('btnClick').addEventListener('click', fetchAndDrawCat);
           scanned_data[poz_m+3]=c;
         }
         }
-        
+
         img_data.data = scanned_data;
         ctx.putImageData(img_data, 0, 0);
       }
